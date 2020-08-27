197498eb72044ee40815ea9f7107bc1794123864
Martyna <martyna.gozda@gmail.com>
2020-08-27 08:45:53 +0200

    add Program

--- src/Program.js
+++ src/Program.js
@@ -9,11 +9,36 @@ function Program() {
                 <div class="overlayImage"></div>
                 <h2>Rób to, czego pragniesz</h2>
                 <div class="programBoxes">
-                <div class="oneprogramBox"></div>
-                <div class="oneprogramBox"></div>
-                <div class="oneprogramBox"></div>
-                <div class="oneprogramBox"></div>
-            </div>
+                <div class="oneprogramBox">
+                        <i class="fas fa-glass-cheers"></i>
+                        <h4>Party</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. </p>
+                <button class="btn">Imprezuj</button>
+                    </div>
+                    <div class="oneprogramBox">
+                    <i class="fas fa-globe-europe"></i>
+                        <h4>Explore</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. </p>
+                <button class="btn">Zwiedzaj</button>
+                    </div>
+               
+                    <div class="oneprogramBox">
+                    <i class="fas fa-music"></i>
+                        <h4>Festival</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. </p>
+                <button class="btn">Baw się</button>
+                    </div>
+                    <div class="oneprogramBox">
+                    <i class="fas fa-users"></i>
+                        <h4>Family</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. </p>
+                <button class="btn">Bądź z rodziną</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
