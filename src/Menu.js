192c2f94b1331b9dbd7d0da49f485bbb059bb851
Martyna <martyna.gozda@gmail.com>
2020-09-07 11:57:05 +0200

    RWD update

--- src/Menu.js
+++ src/Menu.js
@@ -4,19 +4,24 @@ import './Menu.scss';
 
 function Menu() {
     return (
-    <div id="menuContainer">
-        <ul class="menuList">
-            <li class="menuItems">Nasze wyjazdy</li>
-            <li class="menuItems">Okres specjalny</li>
-            <li class="menuItems">Promocje</li>
-            <li class="menuItems">Dlaczego My?</li>
-            <li class="menuItems">O nas</li>
-            <li class="menuItems">FAQ</li>
-            <li class="menuItems">Kontakt</li>
-            <li class="menuItems">Zaloguj</li>
-            <li class="menuItems">PL<i class="arrow down"></i></li>
-        </ul>
-    </div>
+        <div id="menuContainer">
+            <div class="hamburger">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+            <ul class="nav-items">
+                <li class="nav-item">Nasze wyjazdy</li>
+                <li class="nav-item">Okres specjalny</li>
+                <li class="nav-item">Promocje</li>
+                <li class="nav-item">Dlaczego My?</li>
+                <li class="nav-item">O nas</li>
+                <li class="nav-item">FAQ</li>
+                <li class="nav-item">Kontakt</li>
+                <li class="nav-item">Zaloguj</li>
+                <li class="nav-item">PL<i class="arrow down"></i></li>
+            </ul>
+        </div>
     );
 }
 
